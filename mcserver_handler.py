9ba024abb0877f2d42cf62459dccc53bc6c49c0a
Nilava Saha <nilavasaha10@gmail.com>
2024-02-29 17:50:31 -0500

    a

--- mcserver_handler.py
+++ mcserver_handler.py
@@ -15,7 +15,7 @@ def run_server(ctx):
     if process is None:
         try:
             command = f"xterm -hold -e {path}"  # Redirect both stdout and stderr to the file
-            process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE)
+            process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             process_id = process.pid
             print(f"Server Started PID: {process_id}")
             return True
@@ -32,28 +32,11 @@ def stop_server():
     if process is not None:
         try:
             # Send "stop" command to the server
-            send_command("stop\n")
-            process.wait()  # Wait for the subprocess to finish
+            process.terminate()  # Wait for the subprocess to finish
             process = None
             return True
         except Exception as e:
             print(f"Error stopping server: {e}")
-            return False
-        finally:
-            process = None
-            stop_event.set()
-    else:
-        return False
-
-def send_command(command):
-    global process
-    if process is not None:
-        try:
-            process.stdin.write(command.encode('utf-8'))
-            process.stdin.flush()
-            return True
-        except Exception as e:
-            print(f"Error sending command: {e}")
             return False
     else:
         return False
