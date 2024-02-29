567ee1d4567dc315f916b8763f0dcf23cfda9e83
Nilava Saha <nilavasaha10@gmail.com>
2024-02-29 18:01:16 -0500

    a

--- mcserver_handler.py
+++ mcserver_handler.py
@@ -14,8 +14,8 @@ def run_server(ctx):
     global process
     if process is None:
         try:
-            command = f"gnome-terminal -- {path}"  # Redirect both stdout and stderr to the file
-            process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            command = f"./{path}"  # Redirect both stdout and stderr to the file
+            process = subprocess.Popen(command, shell=True, check=True)
             process_id = process.pid
             print(f"Server Started PID: {process_id}")
             return True
